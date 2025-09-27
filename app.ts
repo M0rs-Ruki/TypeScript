fe87240aa873f9df810d7f869ea6e94939652cc3
Anup Pradhan <anuppradhan929@gmail.com>
2025-09-27 10:33:16 +0530

    Add initial type definitions and examples in app.ts

---
+++ app.ts
@@ -0,0 +1,19 @@
+// primitive and reference type
+
+// [], {}, () are reference type
+// primitive type are number, string, boolean, null, undefined, symbol, bigint
+
+
+// 12 // primitive
+// harsh // primitive
+// sharma // primitive
+// 12.77 // primitive
+// true // primitive
+// [] // reference
+// {} // reference
+// "Apple" // primitive
+// false // primitive
+// () // reference
+
+let ale = 12; // primitive
+let a = true // primitive
