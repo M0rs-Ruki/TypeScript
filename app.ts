1f816f7403b516743f8708bd03d0fe5c6875b67d
Anup Pradhan <anuppradhan929@gmail.com>
2025-09-27 10:59:34 +0530

    Refactor app.ts to include additional type examples and comments for clarity

--- app.ts
+++ app.ts
@@ -15,5 +15,77 @@
 // false // primitive
 // () // reference
 
-let ale = 12; // primitive
-let a = true // primitive
+// let ale = 12; // primitive
+// let a = true // primitive
+
+/////////////////////////////////
+
+// let a = 12;
+
+// let arr = [1, 2, 3, 'mors', true];
+
+// let arr2: number[] = [1, 2, 3, 4,];
+
+
+// let arr: [string, number] = ['mors', 12]
+
+// // enumerations
+// enum UserRoals {
+//     ADMIN = 'admin',
+//     USER = 'user',
+//     GUEST = 'guest',
+// }
+
+// UserRoals.ADMIN
+
+// Any , Unknown, Void & More
+// let a: number;
+// a = 12;
+// a = true; // error
+
+// let a; // any
+// a = 12;
+// a = true;
+// a = 'mors';
+// no error
+
+// let a: unknown;
+// a = 12;
+// a = true;
+// a = 'mors';
+// if (typeof a === 'string') {
+//     a.toUpperCase();
+// }
+
+// function abcd(): void {
+//     console.log('mors')
+// }
+
+// function abc(): string {
+//     return 'mors'
+// }
+
+// function ab(): number {
+//     return 238249849;
+// }
+
+// let a = null;
+// let n: null;
+// n = 12;
+// let nu: string | null;
+// nu = 'mors';
+// nu = null;
+// nu = 23;
+// let and: undefined | number;
+// and = 12;
+// and = undefined;
+// and = 'mors';
+
+
+function abcd(): never {
+    while (true) {
+        console.log('mors')
+    }
+}
+abcd();
+console.log('Mors Code');
