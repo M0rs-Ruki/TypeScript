4b38b289caffdce0557310f114623abcb2315008
M0rs-Ruki <anuppradhan929@gmail.com>
2025-10-02 20:18:23 +0530

    Add initial project setup with TypeScript, Express, and configuration files
    - Created .gitignore to exclude unnecessary files and directories.
    - Added package.json and package-lock.json for project dependencies.
    - Included tsconfig.json for TypeScript configuration.
    - Implemented a basic Express server in app.ts with a simple route.
    - Introduced nodemon.json for development server configuration.

---
+++ project/project2/src/app.ts
@@ -0,0 +1,14 @@
+
+
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+const port: number = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, this is Express + TypeScript');
+});
+
+app.listen(port, () => {
+  console.log(`[Server]: I am running at http://localhost:${port}`);
+});
