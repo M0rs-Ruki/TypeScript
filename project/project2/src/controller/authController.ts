f5af0b7e2501c96df3faeb5fc2dcafc1f8ece048
M0rs-Ruki <anuppradhan929@gmail.com>
2025-10-03 10:41:31 +0530

    Add authentication functionality to authController
    - Implemented login and password comparison logic.
    - Added error handling for missing fields and invalid credentials.
    - Enhanced user registration process with appropriate responses.

--- project/project2/src/controller/authController.ts
+++ project/project2/src/controller/authController.ts
@@ -1,6 +1,19 @@
 
 import type { Request, Response } from 'express'
-import User from '../models/userModels.js'
+import User from '../models/userModels.js';
+
+
+const comparePass = async (password: string, hashedPassword: string) => {
+    try {
+        if (password === hashedPassword) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (error: any) {
+        throw new Error(error);
+    }
+}
 
 const register = async (req: Request, res: Response) => {
     try {
@@ -27,3 +40,37 @@ const register = async (req: Request, res: Response) => {
         res.status(500).json({ message: "An error occurred while registering the user." });
     }
 }
+
+const login = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            res.status(400).json({ message: "Please fill all the fields" });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "User does not exist" });
+        }
+
+        const isPasswordValid = await comparePass(password, user.password)
+        if (!isPasswordValid) {
+            res.status(400).json({ message: "Invalid password" });
+        }
+
+        res.status(200).json({ message: "Login successful" });
+    } catch (error: any) {
+        res.status(500).json({ message: error.message });
+        console.log(error);
+    }
+}
+
+
+
+
+
+
+
+
+
+export { register, login,  };
